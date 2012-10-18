dc72a3ef81c3969a3e8b695e5f68b05b8a4e4325
mac <mac@mactekiiMac-2.local>
2012-10-18 10:36:30 -0300

    Initial Commit

---
+++ Flights/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Flights
+//
+//  Created by mac on 12-10-18.
+//  Copyright (c) 2012年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
